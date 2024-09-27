43e65e210af8021e0779b49fbed512328eba6456
MaliaRomero <romerm15@my.erau.edu>
2024-09-26 21:24:19 -0700

    Initial Commit

---
+++ Decorator/Assets/Weapon.cs
@@ -0,0 +1,37 @@
+namespace Chapter.Decorator
+{
+    public class Weapon : IWeapon
+    {
+        public float Range
+        {
+            get { return _config.Range; }
+        }
+
+        public float Rate
+        {
+            get { return _config.Rate; }
+        }
+
+        public float Strength
+        {
+            get { return _config.Strength; }
+        }
+
+        public float Cooldown
+        {
+            get { return _config.Cooldown; }
+        }
+        /*
+        public float Duration
+        {
+            get {return _config.Duration;}
+        }*/
+
+        private readonly WeaponConfig _config;
+
+        public Weapon(WeaponConfig weaponConfig)
+        {
+            _config = weaponConfig;
+        }
+    }
+}
